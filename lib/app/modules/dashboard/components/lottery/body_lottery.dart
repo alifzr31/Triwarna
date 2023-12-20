cc76bf16e9770327bb245c6b22bb2fe7b0ea7564
alifzr31 <alifzakya.r@gmail.com>
2023-12-20 15:43:58 +0700

    fixing lottery design

--- lib/app/modules/dashboard/components/lottery/body_lottery.dart
+++ lib/app/modules/dashboard/components/lottery/body_lottery.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:triwarna_rebuild/app/components/base_tabbar.dart';
 import 'package:triwarna_rebuild/app/components/base_text.dart';
-import 'package:triwarna_rebuild/app/core/values/colors.dart';
 import 'package:triwarna_rebuild/app/modules/dashboard/components/lottery/my_lottery.dart';
 import 'package:triwarna_rebuild/app/modules/dashboard/components/lottery/winner_lottery.dart';
 import 'package:triwarna_rebuild/app/modules/dashboard/controller.dart';
@@ -19,18 +19,8 @@ class BodyLottery extends StatelessWidget {
           initialIndex: controller.currentTabLottery.value,
           child: Column(
             children: [
-              TabBar(
+              BaseTabBar(
                 onTap: (index) => controller.currentTabLottery.value = index,
-                labelPadding: const EdgeInsets.symmetric(vertical: 10),
-                indicatorSize: TabBarIndicatorSize.tab,
-                unselectedLabelStyle: const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400,
-                ),
-                labelStyle: const TextStyle(
-                  color: purpleColor,
-                  fontWeight: FontWeight.w600,
-                ),
                 tabs: const [
                   BaseText(text: 'Undian Saya'),
                   BaseText(text: 'Pemenang Undian'),
