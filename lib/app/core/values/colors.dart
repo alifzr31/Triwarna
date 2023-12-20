2dda7568b9cfec26ce646676816c5850cca8dab8
alifzr31 <alifzakya.r@gmail.com>
2023-12-20 16:13:40 +0700

    .

--- lib/app/core/values/colors.dart
+++ lib/app/core/values/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color purpleColor = Color(0xFF493169);
 const Color yellowColor = Color(0xFFFFBD00);
-const Color softPurpleColor = Color(0xFFDAD5E1);
+const Color softPurpleColor = Color(0xFFf2edf9);
 const Color baseBgScaffold = Color(0xFFF3F3F3);
 const Color silverSolidLabel = Color(0xFFC3C3C3);
 const Color goldSolidLabel = Color(0xFFD09A00);
