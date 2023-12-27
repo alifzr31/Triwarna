ca0a7dcc8c606c1bbb4ad756c599c59003c1a4eb
alifzr31 <alifzakya.r@gmail.com>
2023-12-27 15:06:38 +0700

    .

--- lib/app/data/providers/auth_provider.dart
+++ lib/app/data/providers/auth_provider.dart
@@ -3,10 +3,6 @@ import 'package:triwarna_rebuild/app/core/utils/api_url.dart';
 import 'package:triwarna_rebuild/app/core/utils/dio_client.dart';
 
 class AuthProvider extends DioClient {
-  Future<Response> verify(FormData formData) async {
-    return await post(EndPoint.verify, data: formData);
-  }
-  
   Future<Response> logout() async {
     return await post(EndPoint.logout);
   }
