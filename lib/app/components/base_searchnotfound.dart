8c4f7335b5ac78d7807f101831df9948cfeb92e4
alifzr31 <alifzakya.r@gmail.com>
2023-12-27 15:35:15 +0700

    add search not found component

---
+++ lib/app/components/base_searchnotfound.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:triwarna_rebuild/app/components/base_text.dart';
+
+class BaseSearchNotFound extends StatelessWidget {
+  const BaseSearchNotFound({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset(
+              'assets/lotties/winner_notfound.json',
+              width: 180,
+              frameRate: FrameRate(144),
+              repeat: false,
+            ),
+            BaseText(
+              text: title,
+              textAlign: TextAlign.center,
+              size: 16,
+              bold: FontWeight.w500,
+            ),
+            BaseText(
+              text: subtitle,
+              textAlign: TextAlign.center,
+              color: Colors.grey.shade600,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
