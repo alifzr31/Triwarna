16cf73b6bdc926e5667b404b6368a2bffa48faff
alifzr31 <alifzakya.r@gmail.com>
2024-01-10 16:52:09 +0700

    .

--- lib/app/core/values/custom_bottomsheet.dart
+++ lib/app/core/values/custom_bottomsheet.dart
@@ -4,6 +4,14 @@ import 'package:get/get.dart';
 void customBottomSheet(double? height, Widget child) {
   Get.bottomSheet(
     backgroundColor: Colors.white,
+    clipBehavior: Clip.antiAlias,
+    
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(15),
+        topRight: Radius.circular(15),
+      ),
+    ),
     SizedBox(
       height: height,
       width: Get.width,
